c63c9176acedf731b3657c77258ca703e0d1e8c2
User <User@USER->
2021-07-11 19:07:50 +0300

    Add project files.

---
+++ HomeTask-01.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HomeTask_01._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            string myText = Console.ReadLine();
+
+            char[] splitted = myText.ToCharArray();
+            int[] notSorted = new int[3];
+            notSorted[0] = int.Parse(splitted[0].ToString());
+            notSorted[1] = int.Parse(splitted[1].ToString());
+            notSorted[2] = int.Parse(splitted[2].ToString());
+
+            Array.Sort(notSorted);
+            Console.WriteLine($"{notSorted[2]}{notSorted[1]}{notSorted[0]}");
+        }
+    }
+}
